747efe3f66bca674926da03885f55211013098ba
nileshrn01 <146316325+r-nilesh-c@users.noreply.github.com>
2025-03-09 14:53:07 +0530

    Update CustomByteCipher.java

--- src/main/java/com/yourproject/utils/CustomByteCipher.java
+++ src/main/java/com/yourproject/utils/CustomByteCipher.java
@@ -1,52 +1,31 @@
 package com.yourproject.utils;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.List;
 
 public class CustomByteCipher {
-    private static final SecureRandom secureRandom = new SecureRandom();
-    private static final int KEY_INTERVAL = 128;
-    private static final int KEY_DERIVATION_ROUNDS = 3;
+    private static final String ALGORITHM = "AES";
+    private static final int KEY_SIZE = 128;
 
-    private static List<Integer> expandKey(int initialKey, int length) {
-        List<Integer> expandedKeys = new ArrayList<>();
-        int currentKey = initialKey;
-        for (int i = 0; i < length; i++) {
-            currentKey = (currentKey * 0x3498) % 0xff;
-            currentKey = (currentKey ^ (i * 0x7621)) % 0xff;
-            expandedKeys.add(currentKey);
-        }
-        return expandedKeys;
+    private static byte[] generateKey(int initialKey) throws Exception {
+        byte[] key = new byte[KEY_SIZE / 8];
+        System.arraycopy(String.valueOf(initialKey).getBytes(), 0, key, 0, key.length);
+        return key;
     }
 
     public static byte[] encrypt(byte[] data, int initialKey) throws Exception {
-        byte[] encryptedData = new byte[data.length];
-        List<Integer> expandedKey = expandKey(initialKey, data.length);
-
-        for (int i = 0; i < data.length; i++) {
-            int key = expandedKey.get(i);
-            int encryptedByte = (data[i] + key) % 256;
-            encryptedByte = (encryptedByte ^ (key * 0x23)) % 256;
-            encryptedData[i] = (byte) encryptedByte;
-        }
-
-        return encryptedData;
+        SecretKey secretKey = new SecretKeySpec(generateKey(initialKey), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        return cipher.doFinal(data);
     }
 
     public static byte[] decrypt(byte[] encryptedData, int initialKey) throws Exception {
-        byte[] decryptedData = new byte[encryptedData.length];
-        List<Integer> expandedKey = expandKey(initialKey, encryptedData.length);
-
-        for (int i = 0; i < encryptedData.length; i++) {
-            int key = expandedKey.get(i);
-            int decryptedByte = (encryptedData[i] & 0xFF);
-            decryptedByte = (decryptedByte ^ (key * 0x23)) % 256;
-            decryptedByte = (decryptedByte - key + 256) % 256;
-            decryptedData[i] = (byte) decryptedByte;
-        }
-        return decryptedData;
+        SecretKey secretKey = new SecretKeySpec(generateKey(initialKey), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        return cipher.doFinal(encryptedData);
     }
 }
